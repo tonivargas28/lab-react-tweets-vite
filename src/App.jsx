1cecb644c56deb3a48653e9a1812cf60564d543a
tonivargas28 <antoniovargasgarcia21@gmail.com>
2025-03-27 20:17:08 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,43 @@
-import "./App.css";
+import React from "react";
 import Tweet from "./components/Tweet";
+import "./index.css";
 
 const tweetsArray = [
   {
     user: {
-      name: "Thoughts of Dog®",
-      image:
-        "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/m3/tweet-user-photo.png",
-      handle: "dog_feelings",
-    },
-    timestamp: "1h ago",
-    message:
-      "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
-  },
-  {
-    user: {
-      name: "Thoughts of Dog®",
-      image:
-        "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/m3/tweet-user-photo.png",
-      handle: "dog_feelings",
+      name: "Antonio Vargas",
+      image: "https://i.pravatar.cc/150?img=3",
+      handle: "@antonio"
     },
     timestamp: "2h ago",
-    message:
-      "sometimes. the human presses their noggin against mine. to figure out what i’m thinking. so i just think really hard. about how much i love them. and hope they figure it out",
+    message: "Este es mi primer tweet!"
   },
   {
     user: {
-      name: "Thoughts of Dog®",
-      image:
-        "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/m3/tweet-user-photo.png",
-      handle: "dog_feelings",
+      name: "Maria López",
+      image: "https://i.pravatar.cc/150?img=5",
+      handle: "@maria"
     },
-    timestamp: "3h ago",
-    message:
-      "here is what. i plan to accomplish today: \n\n2. bark loudly. but at nothing \n7. lose my ball under the couch\n7b. politely ask the human. to get my ball\n3. immediately lose it again. under the same couch\n4. big nap. you have worked hard\n2. repeat",
+    timestamp: "1h ago",
+    message: "React es increíble!"
   },
+  {
+    user: {
+      name: "Carlos Pérez",
+      image: "https://i.pravatar.cc/150?img=8",
+      handle: "@carlos"
+    },
+    timestamp: "30m ago",
+    message: "¿Alguien más está practicando React?"
+  }
 ];
 
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((tweet, index) => (
+        <Tweet key={index} tweet={tweet} />
+      ))}
     </div>
   );
 }
